b2217d55060a319cbcf0174a2b177579b286c610
claudiowerner <40247488+claudiowerner@users.noreply.github.com>
2021-07-10 00:12:31 -0400

    Cambio 021
    - Al hacer clic en algún ítem del ListView de la ActivityNomina.java se envía el número de documento tributario obtenido a través del método tokenizer

--- DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/activityFacturaNomina.java
+++ DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/activityFacturaNomina.java
@@ -3,12 +3,28 @@ package com.example.despachoferreteriaswernerraddatz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activityFacturaNomina extends AppCompatActivity {
+
+    //declaración de objetos
+
+    TextView txtNumDoc, txtNumPaquetesEntregados;
+    EditText edtComentario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_factura_nomina);
+
+        txtNumDoc = findViewById (R.id.txtNumDoc);
+        txtNumPaquetesEntregados = findViewById (R.id.txtNumPaquetesEntregados);
+        edtComentario = findViewById (R.id.edtComentario);
+
+        //recepción de valor enviado desde el ActivityNomina.java
+        Bundle bundle = getIntent().getExtras();
+        String numDoc = getIntent().getStringExtra("codigo_barra");
+        txtNumDoc.setText ("Documento no.\n"+numDoc);
     }
 }
