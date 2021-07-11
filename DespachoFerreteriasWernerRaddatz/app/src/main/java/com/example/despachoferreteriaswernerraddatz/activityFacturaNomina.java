b800e367b0b833cbeb2825d99c6026fa4a2a551f
claudiowerner <40247488+claudiowerner@users.noreply.github.com>
2021-07-11 14:07:05 -0400

    Cambio 021
    - La App permite guardar comentarios

--- DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/activityFacturaNomina.java
+++ DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/activityFacturaNomina.java
@@ -2,15 +2,20 @@ package com.example.despachoferreteriaswernerraddatz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.despachoferreteriaswernerraddatz.baseDatosSQLite.ConnectionSQLiteHelper;
+import com.example.despachoferreteriaswernerraddatz.funciones.Funciones;
 
 import java.util.ArrayList;
 
@@ -20,6 +25,10 @@ public class activityFacturaNomina extends AppCompatActivity {
 
     TextView txtNumDoc, txtNumPaquetesEntregados;
     EditText edtComentario;
+    Button btnGuardarComentario;
+
+    //llamada a clase funciones
+    Funciones fun = new Funciones ();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +38,28 @@ public class activityFacturaNomina extends AppCompatActivity {
         txtNumDoc = findViewById (R.id.txtNumDoc);
         txtNumPaquetesEntregados = findViewById (R.id.txtNumPaquetesEntregados);
         edtComentario = findViewById (R.id.edtComentario);
+        btnGuardarComentario = findViewById (R.id.btnGuardarComentario);
 
         //recepción de valor enviado desde el ActivityNomina.java
-        Bundle bundle = getIntent().getExtras();
         String numDoc = getIntent().getStringExtra("codigo_barra");
         txtNumDoc.setText ("Documento no.\n"+numDoc);
         int paq_entr = num_paquetes_entregados (numDoc);//captura el numero de paquetes entregados
         int paq_carga = num_paquetes_totales (numDoc);//obtiene el numero de paquetes existentes en el camión
         int porc_avance = (paq_entr/paq_carga)*100;
         txtNumPaquetesEntregados.setText ("Entregados "+paq_entr+"/"+paq_carga+" ("+porc_avance+"%)");
+
+        //recepción de comentario
+        edtComentario.setText (comentario (numDoc));
+
+
+        //acciones guardar comentario
+        btnGuardarComentario.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                guardarComentario (numDoc,edtComentario.getText ().toString ());
+            }
+        });
+
     }
 
     //Obtiene el número de paquetes entregados
@@ -63,6 +85,7 @@ public class activityFacturaNomina extends AppCompatActivity {
     private int num_paquetes_totales(String numDoc)
     {
         int i = 0;//captura el número de paquetes entregados
+        //objetos de la base de datos
         //llamada a clase ConnectionSQLiteHelper
         ConnectionSQLiteHelper conn = new ConnectionSQLiteHelper (this, "bd_interna_despacho_wyr", null, 1);
 
@@ -70,11 +93,42 @@ public class activityFacturaNomina extends AppCompatActivity {
 
         Cursor cursor = db.rawQuery("select * from caja_estatus_reporte where estatus=3 and num_doc = '"+numDoc+"'",null);
 
-        ArrayList array = new ArrayList ();
         while (cursor.moveToNext ())
         {
             i++;
         }
         return i;
+    }
+
+    //obtiene el comentario del número de documento seleccionado
+    private String comentario (String numDoc)
+    {
+        //objetos de la base de datos
+        //llamada a clase ConnectionSQLiteHelper
+        ConnectionSQLiteHelper conn = new ConnectionSQLiteHelper (this, "bd_interna_despacho_wyr", null, 1);
+
+        SQLiteDatabase db = conn.getReadableDatabase ();
+        Cursor cursor = db.rawQuery("select * from caja_estatus_reporte where estatus=3 and num_doc = '"+numDoc+"'",null);
+
+        ArrayList array = new ArrayList ();
+        while (cursor.moveToNext ())
+        {
+            return cursor.getString (5);
+        }
+        return "";
+    }
+
+
+    private void guardarComentario(String numDoc, String comentario)
+    {
+        //agregar comentario a paquete no entregado
+        //objetos de la base de datos
+        //llamada a clase ConnectionSQLiteHelper
+        ConnectionSQLiteHelper conn = new ConnectionSQLiteHelper (this, "bd_interna_despacho_wyr", null, 1);
+
+        SQLiteDatabase db = conn.getReadableDatabase ();
+        db.execSQL ("update caja_estatus_reporte set comentario = '"+comentario+"' where num_doc = '"+numDoc+"' and estatus = 3 or estatus = 4");
+
+        fun.dialogoAlerta (this, "¡Aviso!","Comentario ingresado exitosamente");
     }
 }
