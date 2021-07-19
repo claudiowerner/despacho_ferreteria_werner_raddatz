13c2cb0ff56ce4692bc0dda5c8b93551be045bbe
claudiowerner <40247488+claudiowerner@users.noreply.github.com>
2021-07-19 16:48:23 -0400

    Cambio 033
    - Se cambia el metodo registrarEmpleado() a la clase ActivityPrimerUso.java porque la app se caía al intentar ejecutar un AlertDialog

--- DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/ActivityPrimerUso.java
+++ DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/ActivityPrimerUso.java
@@ -2,7 +2,9 @@ package com.example.despachoferreteriaswernerraddatz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -11,7 +13,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.despachoferreteriaswernerraddatz.baseDatosSQLite.ConnectionDB;
 import com.example.despachoferreteriaswernerraddatz.baseDatosSQLite.ConnectionSQLiteHelper;
+import com.example.despachoferreteriaswernerraddatz.baseDatosSQLite.CrudBDExterna;
 import com.example.despachoferreteriaswernerraddatz.baseDatosSQLite.CrudBDInterna;
 import com.example.despachoferreteriaswernerraddatz.funciones.Funciones;
 
@@ -24,6 +34,9 @@ public class ActivityPrimerUso extends AppCompatActivity {
     //creación de objetos
     EditText edtCodigo, edtNombre, edtApellido;
     Button btnRegistrar;
+
+    //llamada a clase funciones
+    Funciones fun = new Funciones();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +51,6 @@ public class ActivityPrimerUso extends AppCompatActivity {
 
         //llamada a clase Funciones
         Funciones fun = new Funciones();
-
-
-
 
         //llamada a creación de base de datos. En este caso la BD no se creará, ya que existe.
         ConnectionSQLiteHelper conn = new ConnectionSQLiteHelper (this, "bd_interna_despacho_wyr", null, 1);
@@ -57,17 +67,72 @@ public class ActivityPrimerUso extends AppCompatActivity {
                 id_dispositivo = fun.obtenerAndroidID (getApplicationContext ());
                 marca_modelo_dispositivo = fun.obtenerMarcaDispositivo ()+" "+fun.obtenerModeloDispositivo ();
 
-                //llamada a método de registro
-                CrudBDInterna crud = new CrudBDInterna ();
-                crud.registrarEmpleado (db,getApplicationContext (),codigo,nombre,apellido,id_dispositivo,marca_modelo_dispositivo);
+                if(codigo.equals ("")||nombre.equals ("")|| apellido.equals (""))
+                {
+                    fun.dialogoAlerta (ActivityPrimerUso.this, "¡Aviso!","Debe rellenar todos los campos");
+                }
+                else
+                {
 
+                    //llamada a la clase de registros en BD Interna del dispositivo
+                    CrudBDInterna crudBDInterna = new CrudBDInterna ();
+                    //Registro de datos en la base de datos interna
+                    crudBDInterna.registrarEmpleado (db,getApplicationContext (),codigo,nombre,apellido,id_dispositivo,marca_modelo_dispositivo);
 
+                    //llamada a la clase de registros en la BD remota de la empresa
+                    CrudBDExterna crudBDExterna = new CrudBDExterna ();
+                    registrarEmpleado(id_dispositivo,marca_modelo_dispositivo,codigo,nombre,apellido);
 
-                Intent intent = new Intent(getApplicationContext (), MainActivity.class);
-                intent.addCategory(Intent.CATEGORY_HOME);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                    //llamada a la activity
+                    Intent intent = new Intent(getApplicationContext (), MainActivity.class);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
             }
         });
+    }
+    public void registrarEmpleado(String id_dispositivo, String marca_modelo, String id_empleado, String nombre, String apellido)//método que sirve para buscar e imprimir los datos de los clientes
+    {
+        ConnectionDB db = new ConnectionDB ();
+        final String[][] matriz = {new String[0]};
+
+        //Esta URL enviará los datos al servicio web create_empleado.php
+        String url = db.host () + "create/create_empleado.php?id_dispositivo="+id_dispositivo+
+                "&marca_modelo="+marca_modelo+
+                "&id_empleado="+id_empleado+
+                "&nombre="+nombre+
+                "&apellido="+apellido;
+        RequestQueue queue = Volley.newRequestQueue (this);
+        StringRequest stringRequest = new StringRequest (Request.Method.GET, url, new Response.Listener<String> () {
+            @Override
+            public void onResponse(String response) {
+                if (response.length () == 1) {
+                    fun.dialogoAlerta (getApplicationContext (), "Oops...", "Ocurrió un error al intentar autorizar el uso de esta aplicación en su dispositivo.");
+                }
+                else
+                {
+                    dialogoAlerta ("Enhorabuena", "Autorización exitosa para operar esta aplicación.");
+                }
+            }
+        }, new Response.ErrorListener () {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Toast.makeText (getApplicationContext (), "Tiempo de espera agotado. De igual manera, el registro se hizo de manera local", Toast.LENGTH_LONG).show ();
+                System.out.println ("Error de Volley; "+error);
+            }
+        });
+        queue.add (stringRequest);
+    }
+    public void dialogoAlerta(String titulo, String mensaje)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(titulo);
+        builder.setMessage(mensaje);
+        builder.setPositiveButton("Aceptar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/baseDatosSQLite/CrudBDExterna.java
+++ DespachoFerreteriasWernerRaddatz/app/src/main/java/com/example/despachoferreteriaswernerraddatz/baseDatosSQLite/CrudBDExterna.java
@@ -3,6 +3,7 @@ package com.example.despachoferreteriaswernerraddatz.baseDatosSQLite;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -28,74 +29,34 @@ public class CrudBDExterna
     /**llamada a la clase Funciones*/
     Funciones fun = new Funciones ();
 
-    public void listarDatosEscaneados(Context context, Drawable drawable, ListView lst)//método que sirve para buscar e imprimir los datos de los clientes
+    public void registrarDatosEscaneados(Context context,
+                                         String cod_barra,
+                                         String estatus,
+                                         String num_doc,
+                                         String fecha,
+                                         String hora,
+                                         String comentario,
+                                         String id_dispositivo)//método que sirve para registrar los enviados por el dispositivo Android
     {
-        //progressDialog
-        final ProgressDialog progressDialog;
-        progressDialog = new ProgressDialog (context);
-        progressDialog.setIcon (drawable);
-        progressDialog.setMessage ("Cargando...");
-        progressDialog.show ();
-
-        final String[][] matriz = {new String[0]};
-
-        String url = dbConn.host () + "";
-        RequestQueue queue = Volley.newRequestQueue (context);
-        StringRequest stringRequest = new StringRequest (Request.Method.GET, url, new Response.Listener<String> () {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    progressDialog.show ();
-                    ArrayAdapter<String> arrayCliente;
-                    if (response.length () == 1) {
-                        Thread.sleep (1000);
-                        progressDialog.dismiss ();
-                        fun.dialogoAlerta (context, "Búsqueda", "No existen datos");
-                        arrayCliente = new ArrayAdapter<String> (context, android.R.layout.simple_expandable_list_item_1, matriz[0]);
-                        lst.setAdapter (arrayCliente);
-                    } else {
-                        Thread.sleep (1000);
-                        progressDialog.dismiss ();
-                        try {
-                            JSONArray arr = new JSONArray (response);
-                            matriz[0] = new String[arr.length ()];
-                            for (int i = 0; i < arr.length (); i++) {
-                                matriz[0][i] = arr.getString (0);
-                            }
-                            arrayCliente = new ArrayAdapter<String> (context, android.R.layout.simple_expandable_list_item_1, matriz[0]);
-                            lst.setAdapter (arrayCliente);
-                        } catch (JSONException e) {
-                            //int err = e.getCause().hashCode();
-                            fun.dialogoAlerta (context, "Error", "No se pueden mostrar los datos requeridos. \nCausa: " + e.getCause ());
-                            e.printStackTrace ();
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace ();
-                }
-            }
-        }, new Response.ErrorListener () {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                fun.dialogoAlerta (context, "Error de conexión", "Asegúrese de que su dispositivo esté conectado a Internet.");
-                progressDialog.dismiss ();
-            }
-        });
-        queue.add (stringRequest);
-    }
-
-    public void registrarDatosEscaneados(Context context, String cod_barra, String estatus, String num_doc, String fecha, String hora, String comentario, String id_dispositivo, String marca_modelo, String id_empleado, String nombre_empleado, String apellido_empleado)//método que sirve para registrar los enviados por el dispositivo Android
-    {
-        String url = dbConn.host () + "create/create.php";
+        String url = dbConn.host () + "create/create_caja.php?cod_barra="+cod_barra+
+                "&estatus="+estatus+
+                "&num_doc="+num_doc+
+                "&fecha="+fecha+
+                "&hora="+hora+
+                "&comentario="+comentario+
+                "&id_dispositivo="+id_dispositivo;
         RequestQueue queue = Volley.newRequestQueue (context);
 
         //Los datos se enviarán a través del método GET para evitar la visibilidad de estos
         StringRequest stringRequest = new StringRequest (Request.Method.GET, url, new Response.Listener<String> () {
             @Override
             public void onResponse(String response) {
-                if (response.length () == 1) {
+                if (response.length () == 1)
+                {
                     fun.dialogoAlerta (context, "¡Aviso!", response);
-                } else {
+                }
+                else
+                {
                     //
                 }
             }
